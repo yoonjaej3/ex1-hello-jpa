d814750d26dd21e4faa6beb1fa88c0ce2120f77b
yoonjaej3 <yoonjaej3@gmail.com>
2022-03-31 00:46:46 +0900

    data mapping

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -1,5 +1,8 @@
 package hellojpa;
 
+import hellojpa.domain.Member;
+import hellojpa.domain.Order;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
@@ -17,51 +20,9 @@ public class JpaMain {
         tx.begin();
 
         try{
-            Member member =new Member();
-           // member.setId(1L);
-            member.setUsername("A");
-            em.persist(member);
-            System.out.println("================");
-            System.out.println(member.getId());
-            System.out.println("================");
-           // member.setRoleType(RoleType.USER);
 
-
-//            em.persist(member);
-//            List<Member> result = em.createQuery("select m from Member as m",Member.class)
-//                            .getResultList();
-//
-//            for (Member member : result){
-//                System.out.println("member.name = "+ member.getName());
-//            }
-
-//            //비영속
-//            Member member = new Member();
-//            member.setId(101L);
-//            member.setName("HelloJPA");
-//
-//            //영속
-//            System.out.println("=== BEFORE ===");
-//            em.persist(member);
-//            System.out.println("=== AFTER ===");
-//
-//            Member findMember= em.find(Member.class,101L);
-//
-//            System.out.println("findMember.id=" + findMember.getId());
-//            System.out.println("findMember.id=" + findMember.getName());
-
-//            Member member1 = new Member(150L,"A");
-//            Member member2 = new Member(160L,"B");
-//
-//            em.persist(member1);
-//            em.persist(member2);
-//
-//            System.out.println("====================");
-
-//              Member member= em.find(Member.class,150L);
-//              member.setName("777777");
-//
-//            System.out.println("====================");
+            Order order=em.find(Order.class,1L);
+            Long memberId=order.getMember().getId();
             tx.commit();
         } catch(Exception e){
             tx.rollback();
---
+++ src/main/java/hellojpa/domain/Order.java
@@ -0,0 +1,26 @@
+package hellojpa.domain;
+
+import lombok.Getter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name="ORDERS")
+@Getter
+public class Order {
+
+    @Id @GeneratedValue
+    @Column(name="order_id")
+    private Long id;
+
+    @Column(name="member_id")
+    private Long memberId;
+
+    private Member member;
+
+    private LocalDateTime orderDate;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status;
+}
